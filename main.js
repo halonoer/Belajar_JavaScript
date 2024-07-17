6ffcbddda737fb49fb9d1a1c3ec89f6c64c998e9
halonoer <bekerjanoi123@gmail.com>
2024-07-17 21:52:18 +0700

    belajar function

---
+++ main.js
@@ -0,0 +1,7 @@
+function addName(firstname, lastname) {
+    // console.log(firstname, lastname)
+    console.log('nama depan:', firstname)
+    console.log('nama belakang:', lastname)
+}
+
+addName('Muhammad', 'Noer')
