f52df29a0822bc6e4e850a69d0abaff6992c26ba
halonoer <bekerjanoi123@gmail.com>
2024-07-20 17:07:41 +0700

    nambahin looping part 2

--- Looping.js
+++ Looping.js
@@ -2,6 +2,8 @@
 // ? for, for Of, for in, foreach 
 // * while & do while 
 
+// looping part 1
+
 for(let i = 1; i <= 10; i++){
     // console.log('data ke - ', i )
     if (i % 2 !== 0) console.log('data ke - ', i )
@@ -21,4 +23,26 @@ do {
     console.log('data y ke -', y)
     y++
 } while(y<=10)
-// 05:00
+
+// ! Perulangan di javascript
+// ? for, for Of, for in, foreach 
+// * while & do while 
+// ! MAP
+
+// looping part 2
+
+const pacarsaya = ['agnes', 'putri', 'angela', 'vexana', 'kity']
+
+console.log(pacarsaya)
+
+for(let pacar of pacarsaya){
+    console.log(pacar)
+}
+
+for(let pacar in pacarsaya){
+    console.log(pacar)
+}
+
+pacarsaya.forEach((pacar, index) => {
+    console.log(pacar, index)
+});
